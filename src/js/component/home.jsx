1deac819cbccb6e8ce5c5d4d85097b22a0d79f5a
Alex Perez Rubio <71727357+alex5perez@users.noreply.github.com>
2024-11-18 21:04:29 +0000

    [Subida de codigo] 📁HOME.jsx: Array para los 0. UseState para el todolist. Funcion para que la frase salga con el Enter. Funcion para eliminar una de las frases. Div para el todolist, el input y la lista. Revisamos con el length si hay alguna tarea listada. Operadores aritmeticos para que aparezca una cosa u otra segun la lista. Estructura map para guardar los objetos del Todolist. Y el onclick con la funcion de eliminar.
    📁INDEX.CSS
    Estilos para que la X salga en el hover

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import "../../styles/index.css";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [newTodo, setNewTodo] = useState("");
+	const [todoList, setTodoList] = useState([]);
+
+	const handleEnter = (e) => {
+		if (e.key === "Enter") {
+			setTodoList([...todoList, newTodo]);
+			setNewTodo("");
+		}
+	};
+
+	const handleDelete = (index) => {
+		const newList = [...todoList];
+		newList.splice(index, 1);
+		setTodoList(newList);
+	};
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center container">
+			<h1 className="text-center mt-5">TODO LIST</h1>
+			<input className="form-control" placeholder="Añade una tarea"
+				value={newTodo}
+				onChange={(e) => {
+					setNewTodo(e.target.value);
+				}}
+				onKeyDown={handleEnter} />
+			<h3 className="mt-2">Todo: </h3>
+			<ul>
+				{todoList.length === 0 ? (
+					<p>No hay tareas, añadir tareas</p>)
+					: (todoList.map((todo, index) => (
+						<p key={index} style={{ fontSize: "2rem" }}>
+							{todo}
+							<span
+								style={{ color: "red", cursor: "pointer", marginLeft: "1rem" }}
+								onClick={() => handleDelete(index)}>
+								X
+							</span>
+						</p>
+					))
+					)}
+			</ul>
 		</div>
 	);
 };
